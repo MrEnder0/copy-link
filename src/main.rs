12dae49218b4b7aeb73ae554bb58335d587f9cdb
MrEnder0 <endermobtrey@gmail.com>
2023-05-05 21:33:06 -0500

    test1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/<text>")]
+fn index(text: String) -> String {
+    format!("You entered: {}", text)
 }
 
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/hello", routes![index]);
+    let rocket = rocket::build().mount("/", routes![index]);
 
     Ok(rocket.into())
 }
