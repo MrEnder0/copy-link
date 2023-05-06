c9341b720a2de2cde9ff11aa589935ecaa8910a8
MrEnder0 <endermobtrey@gmail.com>
2023-05-05 21:41:16 -0500

    handle bars

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::collections::BTreeMap;
+use handlebars::Handlebars;
+
 #[macro_use]
 extern crate rocket;
 
 #[get("/<text>")]
 fn index(text: String) -> String {
-    format!("You entered: {}", text)
+    let mut handlebars = Handlebars::new();
+    let source = "You entered: {{text}}";
+    let mut data = BTreeMap::new();
+    data.insert("text".to_string(), text);
+    
+    handlebars.render_template(source, &data).unwrap()
 }
 
 #[shuttle_runtime::main]
