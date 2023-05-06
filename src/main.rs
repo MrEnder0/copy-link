c1f59f4ea0d05698864526e0836ce9393e34bfce
MrEnder0 <endermobtrey@gmail.com>
2023-05-06 03:49:02 -0500

    idk something

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 use std::collections::BTreeMap;
 use handlebars::Handlebars;
+use rocket::response::content::RawHtml;
 
 #[macro_use]
 extern crate rocket;
 
 #[get("/<text>")]
-fn index(text: String) -> String {
+fn index(text: String) -> RawHtml<String> {
     let mut handlebars = Handlebars::new();
     handlebars.register_template_file("index", "templates/index.hbs").unwrap();
-
     let mut data = BTreeMap::new();
-    data.insert("text".to_string(), text);
-    
-    handlebars.render("index", &data).unwrap()
+    data.insert("text", text);
+    let handlebars_output = handlebars.render("index", &data).unwrap();
+
+    //render as html with css
+    RawHtml(handlebars_output)
 }
 
-#[shuttle_runtime::main]
-async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
-
-    Ok(rocket.into())
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
